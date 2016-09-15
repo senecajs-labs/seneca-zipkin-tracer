3bed7f9b28dde48b14aa4c8438bb7e07dcfaa3d9
Paolo Chiodi <chiodi84@gmail.com>
2016-09-15 13:09:24 +0200

    first working version

---
+++ zipkin-tracer.js
@@ -0,0 +1,96 @@
+
+var Tracer = require('./zipkin')
+
+function handler (msg, done) {
+  var pin = msg.meta$.pattern
+  if (msg.transport$) {
+    return handle_as_server(this, pin, msg, done)
+  }
+
+  handle_as_client(this, pin, msg, done)
+}
+
+function wrap_add (seneca) {
+  var root = seneca.root
+  var api_add = root.add
+  root.add = function () {
+    api_add.apply(this, arguments)
+
+    var args = seneca.util.parsepattern(this, arguments, 'action:f? actmeta:o?')
+    var pattern = args.pattern
+
+    if (!internal_action(pattern)) {
+      pattern.strict$ = pattern.strict$ || {}
+      pattern.strict$.add = true
+      api_add.call(seneca, pattern, handler)
+    }
+  }
+}
+
+function internal_action (pattern) {
+  // TODO support jsonic version here (i.e. "role:seneca")
+  return pattern.role === 'seneca' ||
+    pattern.role === 'transport' ||
+    pattern.role === 'options' ||
+    pattern.role === 'mesh' ||
+    pattern.init
+}
+
+function override_actions (seneca) {
+  var actions = seneca.private$.actrouter.list()
+  for (var i = 0; i < actions.length; i++) {
+    var action = actions[i]
+
+    if (!internal_action(action.match)) {
+      seneca.add(action.match, handler)
+    }
+  }
+}
+
+function handle_as_client (context, pin, msg, done) {
+  var service = context.private$.optioner.get().tag
+  var trace_data = Tracer.get_child(context.fixedargs.tracer)
+  Tracer.client_send(trace_data, {
+    service: service,
+    name: pin
+  })
+
+  context.fixedargs.tracer = trace_data
+
+  context.prior(msg, function (err, msg) {
+    Tracer.client_recv(trace_data, {
+      service: service,
+      name: pin
+    })
+
+    done(err, msg)
+  })
+}
+
+function handle_as_server (context, pin, msg, done) {
+  var service = context.private$.optioner.get().tag
+  var trace_data = Tracer.get_data(msg.tracer)
+  Tracer.server_recv(trace_data, {
+    service: service,
+    name: pin
+  })
+
+  msg.tracer = context.fixedargs.tracer = trace_data
+
+  context.prior(msg, function (err, msg) {
+    Tracer.server_send(trace_data, {
+      service: service,
+      name: pin
+    })
+
+    done(err, msg)
+  })
+}
+
+function tracer_plugin (options) {
+  var seneca = this
+  override_actions(seneca)
+  wrap_add(seneca)
+}
+
+module.exports = tracer_plugin
