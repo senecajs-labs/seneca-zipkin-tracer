61ea1e1ee96c0e6d6fcacc53f0e3de5c3c485a20
Paolo Chiodi <chiodi84@gmail.com>
2016-10-14 17:34:05 +0200

    add performance tests

---
+++ performance/no-sampling.test.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const Plugin = require('..')
+const Seneca = require('seneca')
+const Bench = require('fastbench')
+
+function action (msg, done) {
+  done(null, {hello: 'world'})
+}
+
+const seneca = Seneca({log: 'silent'})
+  .use(Plugin, {
+    sampling: 1
+  })
+  .add('performance:test', action)
+
+const clean_seneca = Seneca({log: 'silent'})
+  .add('performance:test', action)
+
+console.log('\n\nNo sampling\n\n')
+
+const run = Bench([
+  function without_tracer (done) {
+    clean_seneca.act('performance:test', done)
+  },
+  function with_tracer (done) {
+    seneca.act('performance:test', done)
+  }
+], 10000)
+
+run(run)
